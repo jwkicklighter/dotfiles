e1d70c74f17556a0fa306df6c14f7cb89bd971eb
Jordan Kicklighter <jwkicklighter@gmail.com>
2016-02-14 15:02:59 -0500

    Improve link script to check if existing file is already a symlink

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -3,17 +3,41 @@
 # Links dotfiles into the home directory
 
 FILE=$HOME/.vimrc
-if [ -a $FILE ]
-  then
+# If file exists and is a regular file
+if [ -e $FILE ] && [ ! -L $FILE ]; then
     echo "Moving ~/.vimrc to ~/.vimrc.old"
     mv $FILE $FILE.old
 fi
-ln -s $(pwd)/vim/vimrc $HOME/.vimrc
+# If the file does not exist now
+# (either we moved it or it was not there)
+if [ ! -e $FILE ]; then
+  echo "Linking .$FILE"
+  ln -s $(pwd)/vim/vimrc $HOME/.vimrc
+fi
 
 FILE=$HOME/.zshrc
-if [ -a $FILE ]
-  then
+# If file exists and is a regular file
+if [ -e $FILE ] && [ ! -L $FILE ]; then
     echo "Moving ~/.zshrc to ~/.zshrc.old"
     mv $FILE $FILE.old
 fi
-ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
+# If the file does not exist now
+# (either we moved it or it was not there)
+if [ ! -e $FILE ]; then
+  echo "Linking .$FILE"
+  ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
+fi
+
+FILE=$HOME/.ideavimrc
+# If file exists and is a regular file
+if [ -e $FILE ] && [ ! -L $FILE ]; then
+    echo "Moving ~/.ideavimrc to ~/.ideavimrc.old"
+    mv $FILE $FILE.old
+fi
+# If the file does not exist now
+# (either we moved it or it was not there)
+if [ ! -e $FILE ]; then
+  echo "Linking .$FILE"
+  ln -s $(pwd)/intellij/ideavimrc $HOME/.ideavimrc
+fi
+
