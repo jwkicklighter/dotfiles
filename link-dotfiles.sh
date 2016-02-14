5a20a837bfaf4620a58286b2bde416cd0cb87013
Jordan Kicklighter <jwkicklighter@gmail.com>
2016-02-14 15:24:52 -0500

    Use loops because don't be lazy

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -2,42 +2,26 @@
 #
 # Links dotfiles into the home directory
 
-FILE=$HOME/.vimrc
-# If file exists and is a regular file
-if [ -e $FILE ] && [ ! -L $FILE ]; then
-    echo "Moving ~/.vimrc to ~/.vimrc.old"
-    mv $FILE $FILE.old
-fi
-# If the file does not exist now
-# (either we moved it or it was not there)
-if [ ! -e $FILE ]; then
-  echo "Linking .$FILE"
-  ln -s $(pwd)/vim/vimrc $HOME/.vimrc
-fi
+DIRECTORIES=("vim" "zsh" "intellij")
 
-FILE=$HOME/.zshrc
-# If file exists and is a regular file
-if [ -e $FILE ] && [ ! -L $FILE ]; then
-    echo "Moving ~/.zshrc to ~/.zshrc.old"
-    mv $FILE $FILE.old
-fi
-# If the file does not exist now
-# (either we moved it or it was not there)
-if [ ! -e $FILE ]; then
-  echo "Linking .$FILE"
-  ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
-fi
+for DIRECTORY in "${DIRECTORIES[@]}"; do
+  for FILEPATH in $DIRECTORY/*; do
+    FILENAME=`basename $FILEPATH`
+    DOTFILE=$HOME/.$FILENAME
 
-FILE=$HOME/.ideavimrc
-# If file exists and is a regular file
-if [ -e $FILE ] && [ ! -L $FILE ]; then
-    echo "Moving ~/.ideavimrc to ~/.ideavimrc.old"
-    mv $FILE $FILE.old
-fi
-# If the file does not exist now
-# (either we moved it or it was not there)
-if [ ! -e $FILE ]; then
-  echo "Linking .$FILE"
-  ln -s $(pwd)/intellij/ideavimrc $HOME/.ideavimrc
-fi
+    # If the file exists and is a regular file
+    if [ -e $DOTFILE ] && [ ! -L $DOTFILE ]
+    then
+      echo "Moving ~/.$FILENAME to ~/.$FILENAME.old"
+      mv $DOTFILE $DOTFILE.old
+    fi
+
+    # If the file does not exist now, or never did
+    if [ ! -e $DOTFILE ]
+    then
+      echo "Linking .$FILENAME..."
+      ln -s `pwd`/$FILEPATH $DOTFILE
+    fi
+  done
+done
 
